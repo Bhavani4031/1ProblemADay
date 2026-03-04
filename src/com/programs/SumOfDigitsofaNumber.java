a6090db54dda11dcdd141e6f14660a97688c0982
Bhavani <reddybhavani109@gmail.com>
2026-03-04 20:47:00 +0530

    7 Sum of digits in a number

--- src/com/programs/SumOfDigitsofaNumber.java
+++ src/com/programs/SumOfDigitsofaNumber.java
@@ -5,10 +5,12 @@ public class SumOfDigitsofaNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num = 2563;
-		System.out.println(sumOfDigits(num));
+		int output = sumOfDigits(num);
+		System.out.println(output);
 	}
 
 	public static int sumOfDigits(int num) {
+		
 		int lastdigit = 0;
 		int sum = 0;
 		while(num > 0) {
@@ -16,6 +18,7 @@ public class SumOfDigitsofaNumber {
 			sum = sum + lastdigit;
 			num = num / 10;
 		}
+		
 		return sum;
 	}
 }
